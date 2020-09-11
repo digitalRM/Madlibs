0e2929799816e1526dfc9ba474833c7b8839fc5b
Ruslan M <r1554899@gmail.com>
2020-09-11 23:41:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+food = input("Enter a food item. ")
+car = input("Whats your favroite car?. ")
+place = input("Whats the name of a restarant. ")
+date2 = input("Whats the date?. ")
+name = input("Whats your Name?. ")
+vac = input("Where do you want to go on vacation?")
+
+print(f"Hi my name is {name} and i was driving my {car} on the date {date2} to get my favorite food {food} the place is {place} Im so glad i got {food} because im going on vacation to {vac}")
